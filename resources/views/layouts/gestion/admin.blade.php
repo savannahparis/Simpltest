fb9fe86ef06be9439375ec87ccde88bc24a7c0d0
savannahparis <savannahparis@hotmail.com>
2019-04-22 12:42:52 -0300

    Add files via upload

--- resources/views/layouts/gestion/admin.blade.php
+++ resources/views/layouts/gestion/admin.blade.php
@@ -115,7 +115,7 @@
 
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h1 mb-0 text-gray-800">Tableau de board</h1>
+            <h1 class="h1 mb-0 text-gray-800"> <a href="{{route('dashboard')}}" title="Revenir au tableau de board">Tableau de board</a></h1>
             
           </div>
           
