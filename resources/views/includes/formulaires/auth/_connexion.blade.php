5e7656a8e4aeab848c821fadf8831426e88c0b9e
savannahparis <savannahparis@hotmail.com>
2019-04-22 11:27:26 -0300

    Add files via upload

--- resources/views/includes/formulaires/auth/_connexion.blade.php
+++ resources/views/includes/formulaires/auth/_connexion.blade.php
@@ -34,7 +34,7 @@
                   </form>
                   <hr>
                   <div class="text-center">
-                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                    
                     
                   </div>
                   
